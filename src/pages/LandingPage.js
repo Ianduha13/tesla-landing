7e771e4b1eda3f46db8b55a53ce6e60a01f60a25
Ian Duhamel <ianduhamelhayes@gmail.com>
2022-12-04 08:48:41 -0300

    Adding Styles

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -16,12 +16,14 @@ const LandingPage = () => {
         <ArrowDown />
       </div>
       <div className='model-s'>
-        <Header header={"Model S"} key={2} />
+        <Header header={"Model S"} key={1} />
         <img
           src='https://tesla-cdn.thron.com/delivery/public/image/tesla/03e533bf-8b1d-463f-9813-9a597aafb280/bvlatuR/std/4096x2560/M3-Homepage-Desktop-LHD'
           alt='model-s'
           className='model-s home-page-img'
         />
+        <Buttons />
+        <ArrowDown />
       </div>
     </div>
   )
