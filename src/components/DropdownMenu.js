d2e8904b8c0439cc706ab5b5af0b72701226953e
Ian Duhamel <ianduhamelhayes@gmail.com>
2022-12-11 16:11:47 -0300

    Adding personal info to the dropdown menu

--- src/components/DropdownMenu.js
+++ src/components/DropdownMenu.js
@@ -1,5 +1,6 @@
 import "./styles/dropdown.css"
 import { SiCitrix } from "react-icons/si"
+import { FaGithub, FaLinkedin } from "react-icons/fa"
 const DropdownMenu = ({ isOpen, setOpen }) => {
   return (
     <>
@@ -32,6 +33,24 @@ const DropdownMenu = ({ isOpen, setOpen }) => {
           <li className='option-list'>Support</li>
           <li className='option-list'>Investor Relations</li>
         </ul>
+        <div className='dropdown-footer'>
+          <a
+            className='footer-btn'
+            href='https://github.com/Ianduha13?tab=repositories'
+            target={"_blank"}
+            rel='noreferrer'
+          >
+            <FaGithub size='1em' color='black' />
+          </a>
+          <a
+            className='footer-btn'
+            href='https://www.linkedin.com/in/ian-duhamel/'
+            target={"_blank"}
+            rel='noreferrer'
+          >
+            <FaLinkedin size='1.2em' color='black' />
+          </a>
+        </div>
       </div>
       <div
         className={`dd-overlay ${isOpen}`}
