6de15125c8c70797f801136e9de0b75f713e76b9
Ian Duhamel <ianduhamelhayes@gmail.com>
2022-12-11 11:31:03 -0300

    Adding scroll styles and img sizing

--- src/components/ModelCard.js
+++ src/components/ModelCard.js
@@ -1,13 +1,12 @@
 import ArrowDown from "../components/ArrowDown"
 import Buttons from "../components/Buttons"
 import Header from "../components/Header"
-import "./styles/modelcard.css"
 
 const ModelCard = ({ model, imgUrl, key }) => {
   return (
     <div className='img-modal' key={key}>
-      <Header header={model} />
       <img src={`${imgUrl}`} alt={`${model}`} className='home-page-img' />
+      <Header header={model} />
       <Buttons />
       <ArrowDown />
     </div>
